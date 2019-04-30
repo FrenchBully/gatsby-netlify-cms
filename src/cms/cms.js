7fff40a6509e2a99048ab1b5bfb907b32548d775
Kevin <kvng88@gmail.com>
2019-04-29 22:10:43 -0700

    removed widget import

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,4 @@
 import CMS from 'netlify-cms'
-import * as ColorWidget from "netlify-cms-widget-color";
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
@@ -8,6 +7,3 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
-
-// WIDGETS
-CMS.registerWidget('color', ColorWidget.Control);

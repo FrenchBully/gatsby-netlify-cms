e80cc091f4fb1d0af4ef90fe436cb3e86794fed9
Kevin <kvng88@gmail.com>
2019-04-29 18:19:03 -0700

    register color widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,5 @@
 import CMS from 'netlify-cms'
+import * as ColorWidget from "netlify-cms-widget-color";
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
@@ -7,3 +8,6 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+// WIDGETS
+CMS.registerWidget("color", ColorWidget.Control);
